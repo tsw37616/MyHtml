319509ce4033f5253c6771800cc6b95923a7be52
唐思为 <111188489+tsw37616@users.noreply.github.com>
2024-11-24 22:01:26 +0800

    create counter

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+var count = 0;
+var p = document.getElementById("display");
+var btn = document.getElementById("add");
 function test() {
   alert("test");
+}
+
+function myFunction() {
+  console.log("run myFunction");
+  document.getElementById("demo").innerHTML = "Hello World";
+}
+function plusOne() {
+  count++;
+  p.innerHTML = count;
 }
